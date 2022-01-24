b3ef21c81ce0f9859043f0519f17d37d885188da
Narasimhamarturi <86408591+Narasimhamarturi@users.noreply.github.com>
2022-01-24 14:37:18 +0530

    Update fix_privileges.sh

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 chmod +x /home/ec2-user/spring_war1/* jar
-chmod +x /home/ec2-user/spring_war1/server_start.sh
-chmod +x /home/ec2-user/spring_war1/server_stop.sh
+chmod +x /home/ec2-user/spring_war1/scripts/server_start.sh
+chmod +x /home/ec2-user/spring_war1/scripts/server_stop.sh
